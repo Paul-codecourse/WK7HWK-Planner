c2967a7d4d8c07c409d5f01765201f295d198b1b
Paul <paulasquith150@gmail.com>
2024-01-24 18:35:21 +0000

    non display error fixed

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,11 +1,9 @@
 //today's date and time
-var gradDate = dayjs("1999/02/15").format("YYYY MM d");
-$("#1a").text(gradDate);
 var currentDay = dayjs().format("dddd DD MMM YYYY");
 $("#3a").text(currentDay);
-
+window.onload = mTable()
 //Make table template
-function Table() {
+function mTable() {
       $(document).ready(function() {
     // Generate rows for each hour from 9 am to 5 pm
     for (let hour = 9; hour <= 17; hour++) {
@@ -15,25 +13,40 @@ function Table() {
                                     <td><a class="task-link" href="#">Click to Add Task</a></td>
                                     <td></td>
                                   </tr>`);
-                                  console.log("this is line 18")
     }
-})}
-/*
+})
 
+$(document).ready(function() {      
+  $('#makeTable').click(function() { 
+     mTable(); 
+  }); 
+});
+
+//onclicking the task link add the text to the cell
+$(".task-link").on("click", function() {
+    // Replace the link with a text input for task entry
+    let inputElement = $("<input type='text' class='task-input' />");
+    $(this).replaceWith(inputElement);}
+ )
+}
+
+function createaTable() {
 for (let hour = 9; hour <= 17; hour++) {
-    let time = (hour < 12) ? hour + " AM" : (hour === 12) ? "12 PM" : (hour - 12) + " PM";
+    let time = hour;
     $("#schedule-body").append(`<tr>
                                   <td>${time}</td>
                                   <td><a class="task-link" href="#">Click to Add Task</a></td>
                                   <td></td>
                                 </tr>`)};
-  }*/
+  }
+
+
 
 function testLog() {
     console.log("This is a test")
 }
 
-
+/*
 
 
 function makeTable() {
@@ -62,4 +75,4 @@ $tbody.append('<tr />').children('tr:last')
 
 // add table to dom
 $table.appendTo('#dynamicTable');
-}
+}*/
